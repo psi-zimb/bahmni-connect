f94fa560b55a8b5ed4b4a3181fd93a2028bb9154
gopikrishna-yaramothu <gopikrishna.y@thoughtworks.com>
2020-08-18 10:37:13 +0530

    Gopi,Som | Spike-PWA | Added logic to get zip file and store data in indexDB

--- ui/app/syncdatarules/app.js
+++ ui/app/syncdatarules/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('syncdatarules', ['ui.router','bahmni.common.offline','bahmni.common.config','bahmni.common.uiHelper', 'ngDialog', 'httpErrorInterceptor','pascalprecht.translate','ng-vkThread'])
+angular.module('syncdatarules', ['ui.router','bahmni.common.offline','bahmni.common.config','bahmni.common.uiHelper', 'ngDialog', 'httpErrorInterceptor','pascalprecht.translate'])
   .config(['$urlRouterProvider', '$stateProvider', '$httpProvider', '$compileProvider', function ($urlRouterProvider, $stateProvider, $httpProvider, $compileProvider) {
     $httpProvider.defaults.headers.common['Disable-WWW-Authenticate'] = true;
     $urlRouterProvider.otherwise('/syncdatarules');
